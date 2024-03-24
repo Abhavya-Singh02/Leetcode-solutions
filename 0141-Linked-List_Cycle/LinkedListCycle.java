6c950310afec38516742d1fb68de09436b66d7c3
Abhavya-Singh02 <abhavya.singh04@gmail.com>
2024-03-24 20:09:24 +0530

    Runtime: 1 ms (23.40%) Memory: 44.25 mb (60.64%)

---
+++ 0141-Linked-List_Cycle/LinkedListCycle.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
